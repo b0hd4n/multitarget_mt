5f96e2b06d0314d0c921eb26c79e8f7e9e612060
Bohdan Ihnatchenko (diplomant <bohdan.ihnatchenko@gmail.com>
2020-04-21 23:30:17 +0200

    Metric agnostic convergence check

--- experiments/en-to-36/converged_models.sh
+++ experiments/en-to-36/converged_models.sh
@@ -1,1 +1,1 @@
-for f in ./model_en2*/valid.log; do ff=(${f//// }); cat $f | grep -m 1 -e 'ce-.*5 times' | sed "s/\(.*\)/${ff[1]} \1/"; done
+for f in ./model_en2*/train.log; do ff=(${f//// }); cat $f | grep -m 1 -e 'Training finished' | sed "s/\(.*\)/${ff[1]} \1/"; done
